fdfbdc8af318dc603b7e2a57a273ff2ca0a16c4b
Simon McNeil <mcneils@sheridancollege.ca>
2019-04-23 16:06:06 -0400

    finished creating basic tableview implementation

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,19 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Simon McNeil on 2019-04-23.
+//  Copyright © 2019 SimonMcNeil. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+    
+    
+    func toggleChecked() {
+        checked = !checked
+    }
+}
