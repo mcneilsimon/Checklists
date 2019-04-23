a14d02669036dba1c21f946964b477ab55428cc9
Simon McNeil <mcneils@sheridancollege.ca>
2019-04-22 21:13:55 -0400

    first commit, and set up basic tableview functionalities

---
+++ Checklists/ChecklistViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Simon McNeil on 2019-04-22.
+//  Copyright © 2019 SimonMcNeil. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    // MARK:- Table View Data Source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        let label = cell.viewWithTag(50) as! UILabel
+        
+        //With the remainder operator every iteration of 5 rows will repeat.
+        if indexPath.row % 5 == 0 {
+            label.text = "Walk the dog"
+        } else if indexPath.row % 5 == 1 {
+            label.text = "Brush my teeth"
+        } else if indexPath.row % 5 == 2 {
+            label.text = "Learn iOS developer"
+        } else if indexPath.row % 5 == 3 {
+            label.text = "Soccer Practice"
+        } else if indexPath.row % 5 == 4 {
+            label.text = "Eat ice cream"
+        }
+        //end of code block
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+        //we use if let because it could be possibe that there is no cell at the specificed index path, for example the row isn't visible
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
